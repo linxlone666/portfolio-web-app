ff5a46d92f27e6bbcd1b55f3dc546698a0256a25
Kasemsak Ratchanipan <gmjohn200gg@gmail.com>
2026-07-01 22:52:21 +0700

    up dete About

---
+++ src/components/About-&-Project.tsx
@@ -0,0 +1,14 @@
+import "./About_Project.css";
+const About_Project: React.FC = () => {
+  return (
+    <div className="main_container">
+      <h1 className="title">About</h1>
+      <div className="in_continer">
+      <img src="https://i.pinimg.com/736x/c1/d2/d2/c1d2d21bc63b0268b5276eed6f892b5b.jpg" alt="About" />
+      <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About_Project;
