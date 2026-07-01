ecdd0f1bbeb0bd77f533e20c0614367e86f6dd00
Darkmather <tanaphoom.phuttong@gmail.com>
2026-07-01 22:39:20 +0700

    add contact & experience

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,4 +1,30 @@
-<div>
-  <h2>Experience</h2>
-  <p>Here is my work experience.</p>
-</div>
+export default function Experience() {
+  return (
+    <section>
+      <section id="title_experience" style={{ textAlign: 'center' }}>
+        <h1>Experience</h1>
+      </section>
+
+      <section
+        id="experience_content"
+        style={{
+          display: 'flex',
+          gap: '24px',
+          marginTop: '24px',
+        }}
+      >
+        <div id="experience_image" style={{marginTop: '20px'}}>
+          <img src="https://placehold.co/200x200" alt="Experience placeholder" />
+        </div>
+
+        <section id="experience_details" style={{ flex: 1, minWidth: '280px' }}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Sit imperdiet lacus tincidunt sagittis lobortis fermentum. Malesuada vestibulum elit curabitur purus ante est faucibus. Facilisi ac dictum quam eu laoreet. Suspendisse nunc mattis suspendisse venenatis tortor mattis porttitor. Magna nisl feugiat congue aliquet tortor gravida egestas quis. Porttitor quis nam ac feugiat aenean enim nibh aliquam mi. Molestie ipsum augue integer sit risus dolor vulputate. Id tincidunt consectetur neque eget viverra. A mauris ridiculus lorem viverra velit feugiat nunc. Leo ultrices eget ullamcorper nunc. Dis urna gravida purus amet adipiscing ipsum mi. Aliquam id velit morbi aliquet adipiscing platea neque augue elementum.
+            Dapibus et euismod ornare feugiat consequat adipiscing. At pellentesque leo in purus. Ante eu congue enim vel neque. Et at risus eget nulla sagittis interdum nulla amet sollicitudin. Venenatis luctus sit nec amet aliquam. Vestibulum cras lacinia eros elit egestas pellentesque odio. Velit porttitor ac amet amet morbi aliquam nullam. Placerat at ridiculus congue est. Iaculis risus enim orci ultricies. Massa praesent gravida odio euismod enim.
+            Odio etiam sit ultrices feugiat. Nunc amet etiam dui aliquet tincidunt aliquam ligula a. Massa duis lorem pellentesque sit vestibulum ac lectus eleifend. Sed ornare aliquet cursus turpis.
+          </p>
+        </section>
+      </section>
+    </section>
+  );
+}
